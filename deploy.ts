759d0e643191d6d8734c2d062c29cb4a6f710b33
Sinh Vu <khacsinh.vu.16091994@gmail.com>
2019-07-16 22:42:34 +0700

    initialize

---
+++ deploy.ts
@@ -0,0 +1,74 @@
+import web3 from 'web3';
+import { XDCValidatorABI } from './abi/xdcValidator';
+import { TransactionReceipt } from 'web3/types';
+
+const Web3 = new web3(new web3.providers.HttpProvider('http://192.168.1.207:8645'));
+
+class Demo {
+  public getBalance () {
+    Web3.eth.getBalance(
+      '0xc300195A5a48Ae53B317DDF90823ee749A027390'
+    ).then((balance: any) => {
+      console.log(`Balance:`, balance.toString());
+    });
+  }
+
+  public propose() {
+    const contract = new Web3.eth.Contract(XDCValidatorABI.abi, '0x0000000000000000000000000000000000000088');
+    const data = contract.methods.propose('0x0104DbDF95782e92bbb82c60a3DA5dC66127cdCb').encodeABI();
+    Web3.eth.accounts.signTransaction({
+      from: '0xc300195A5a48Ae53B317DDF90823ee749A027390',
+      gas: web3.utils.toHex(300000),
+      gasPrice: web3.utils.toWei('4', 'Gwei'),
+      to: '0x0000000000000000000000000000000000000088',
+      value: '10000000000000000000000000',
+      data: data,
+    }, '0x5f15e172b9cf59089739bd2c2b2001897a5e05608cd68419af578eedf6579dfd').then((result) => {
+      console.log(result);
+      Web3.eth.sendSignedTransaction(result.rawTransaction)
+      .on('transactionHash', (hash: String) => {
+        console.log(`txHash`, hash);
+      })
+      .on('receipt', (receipt: TransactionReceipt) => {
+        console.log(`success`, receipt.status);
+      })
+      .on('error', (err: Error) => {
+        console.log(`err`, err);
+      });
+    });
+  }
+
+  public uploadKYC() {
+    const contract = new Web3.eth.Contract(XDCValidatorABI.abi, '0x0000000000000000000000000000000000000088');
+    const data = contract.methods.uploadKYC('hello').encodeABI();
+    Web3.eth.accounts.signTransaction({
+      from: '0xD33DB4D88A1d51c918A2D4e23135225952038e7b',
+      gas: web3.utils.toHex(300000),
+      gasPrice: web3.utils.toWei('4', 'Gwei'),
+      to: '0x0000000000000000000000000000000000000088',
+      data: data,
+    }, '0xfba37211f57258dc455d4643af07da640756caa85c2b1afeeea500c7c3ba5c96').then((result) => {
+      console.log(result);
+      Web3.eth.sendSignedTransaction(result.rawTransaction)
+      .on('transactionHash', (hash: String) => {
+        console.log(`txHash`, hash);
+      })
+      .on('receipt', (receipt: TransactionReceipt) => {
+        console.log(`success`, receipt.status);
+      })
+      .on('error', (err: Error) => {
+        console.log(`err`, err);
+      });
+    });
+  }
+}
+
+const test = new Demo();
+// get balance from address
+// test.getBalance();
+
+// propose new masternodes
+test.propose();
+
+// upload kyc
+// test.uploadKYC();
